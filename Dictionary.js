33af69c650009aa7cb2d87af8b6eacc438d2af2d
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-27 10:02:56 -0700

    lots of JS and changes

---
+++ Dictionary.js
@@ -0,0 +1,87 @@
+
+
+const yes = {
+    name: "yes",
+    partOfSpeech: "noun",
+    definition1: "Used to give an affirmative response.",
+    definition2: "Used as a response to someone addressing one or otherwise trying to attract one's attention.",
+}
+
+const panacea = {
+    name: "panacea",
+    partOfSpeech: "noun",
+    definition1: "An answer or solution for all problems or difficulties.",
+    definition2: "",
+}
+    
+const concatenation = {
+    name:"concatention",
+    partOfSpeech: "noun",
+    definition1: "A series of interconnected or interdependent things or events.",
+    definition2: "",
+}
+
+const saw = {
+    name: "saw",
+    partOfSpeech: "verb",
+    definition1: "Cut (something) using a saw.",
+    definition2: "Make rapid to-and-fro motions in cutting something or in playing a stringed instrument.",
+}
+
+const found = {
+    name: "found",
+    partOfSpeech: "adjective",
+    definition1: "Having been discovered by chance or unexpectedly.",
+    definition2: "(of a ship) Equipped; Supplied.",
+}
+
+const crane = {
+    name: "crane",
+    partOfSpeech: "verb",
+    definition1: "Stretch out one's body or neck in order to see something.",
+    definition2: "Move (a heavy object) with a crane.",
+}
+
+const minute = {
+    name: "minute",
+    partOfSpeech: "noun",
+    definition1: "A period of time equal to sixty seconds or a sixtieth of an hour.",
+    definition2: "A sixtieth of a degree of angular measurement (symbol: ʹ)",
+}
+
+const grotesque = {
+    name: "grotesque",
+    partOfSpeech: "adjective",
+    definition1: "Comically or repulsively ugly or distorted.",
+    definition2: "",
+}
+
+const label = {
+    name: "label",
+    partOfSpeech: "noun",
+    definition1: "A small piece of paper, fabric, plastic, or similar material attached to an object and giving information about it.",
+    definition2: "",
+}
+
+const debacle = {
+    name: "debacle",
+    partOfSpeech: "noun",
+    definition1: "A sudden and ignominious failure; a fiasco.",
+    definition2: "",
+}
+
+const wordList = {
+    "yes" : yes,
+    "panacea" : panacea,
+    "concatenation" : concatenation,
+    "saw" : saw,
+    "found" : found,
+    "crane" : crane,
+    "minute" : minute,
+    "grotesque" : grotesque,
+    "label" : label,
+    "debacle" : debacle,
+}
+
+
+console.log(wordList.minute)
